cc814041bce1301e3bebf2fa202f548aeb9a70e4
Eduardo Henrique Vieira dos Santos <eduuu_santos@hotmail.com>
2017-01-06 16:16:42 -0300

    Add files via upload

---
+++ genepticon.py
@@ -0,0 +1,248 @@
+import neural, math, copy, random, pickle
+
+class Gen:
+	"""docstring for Gen
+	This class is based on Genetic Algorithm to evolve a Network architecture to solve a given problem
+	"""
+
+	def __init__(self, name = "net"+str(random.random()), hIn = 1, hHi = 1, lHi = 1, hOu = 1, pSize = 50):
+		self.name = name
+
+		self.parent1 = None
+		self.parent2 = None
+
+		#Network architecture attributes
+		self.heightInputLayer = hIn 	#qtty of neurons in first layer
+		self.lenHiddenLayer = lHi 	#qtty of hidden layers
+		self.heightHiddenLayer = hHi 	#qtty of neurons in each hidden layer
+		self.heightOutputLayer = hOu 	#qtty of neurons in the output layer
+
+		self.deservedInputs = []	#float list of inputs evaluated between 0 and 1
+		self.deservedOutputs = []	#float list of its corresponding outputs evaluated between 0 and 1
+		self.idealFitness = 0.001	#accetable miss, usually 1%
+		self.mutationRate = 0.5	#mutation rate
+		self.crossoverRate = 0.5
+		self.populationSize = pSize	#population sizes
+		self.population = []	#population of networks
+		self.statistics = []
+	
+	def newRandom(self):
+		#generates a newRandom Network with its neural espicifications
+		net = neural.Network()
+		net = net.newRandom(self.heightInputLayer, self.heightHiddenLayer, self.lenHiddenLayer, self.heightOutputLayer)
+		net = self.mutation(net)
+		return net
+
+	def randomPopulate(self):
+		#generates a new random population to be selected for first
+		self.population = []
+		for i in range(self.populationSize):
+			self.population.append(self.newRandom())
+		return True
+
+	def populate(self, parent1, parent2, size):
+		#generates a new population based on its size and parents
+		newPopulation = []
+		for i in range(size-2):
+			net = neural.Network()
+			net = self.crossover(parent1, parent2)
+			net = self.mutation(net)
+			newPopulation.append(net)
+		newPopulation.append(parent1)
+		newPopulation.append(parent2)
+		self.population = newPopulation
+
+	def crossover(self, net1, net2):
+		#crossover between parents
+		netA = copy.deepcopy(net1)
+		netB = copy.deepcopy(net2)
+		net = netA.newRandom(self.heightInputLayer,self.heightHiddenLayer,self.lenHiddenLayer,self.heightOutputLayer)
+
+		for i in range(len(net.dendriteList)):
+			for j in range(len(net.dendriteList[i].neuronList)):
+				t = random.random()
+				if t < self.crossoverRate:
+					net.dendriteList[i].neuronList[j].weight = netA.dendriteList[i].neuronList[j].weight
+					net.dendriteList[i].neuronList[j].bend = netA.dendriteList[i].neuronList[j].bend
+				else:
+					net.dendriteList[i].neuronList[j].weight = netB.dendriteList[i].neuronList[j].weight
+					net.dendriteList[i].neuronList[j].bend = netB.dendriteList[i].neuronList[j].bend
+				for k in range(len(net.dendriteList[i].neuronList[j].axon)):
+					t = random.random()
+					if t < self.crossoverRate:
+						net.dendriteList[i].neuronList[j].axon[k].weight = netB.dendriteList[i].neuronList[j].axon[k].weight
+						net.dendriteList[i].neuronList[j].axon[k].bend = netB.dendriteList[i].neuronList[j].axon[k].bend
+		return net
+
+	def mutation(self, net1):
+		#apllies mutation over the network
+		net = copy.deepcopy(net1)
+		for i in net.dendriteList:
+			if i != net.dendriteList[0]: #dodge first layer
+				for j in i.neuronList:
+					t = random.random()
+					if t < self.mutationRate:
+						j.randomize()
+					for k in j.axon:
+						t = random.random()
+						if t < self.mutationRate:
+							k.randomize()
+		return net
+
+	def rank(self, net):
+		#Calculates the deviation between the Network Activation Outputs for each Deserved Input to each Deserved Output
+		deviationSum = 0
+		#Tests each Deserved Inputs for each Deserved Output
+		for i in range(len(self.deservedInputs)):
+			#Activates the Network with the deserved input
+			z = net.activate(self.deservedInputs[i])
+			#Compare each output...
+			for j in range(len(z)):
+				major = z[j]
+				minor = float(self.deservedOutputs[i][j])
+				#Compare the distance between the Network Outputs to the Deserved Outputs
+				distance = abs(major - minor)
+				distance = distance**2
+				if distance > self.idealFitness:
+					#punishment if goal is not achieved
+					distance = distance*1000*(len(self.deservedOutputs))#(len(self.deservedOutputs)*(math.sqrt(distance)) + 2) *( distance + len(self.deservedOutputs)*(math.sqrt(distance)*(math.e/2)))#(len(self.deservedOutputs)) + math.sqrt(distance)*(len(self.deservedOutputs))#
+				deviationSum = deviationSum + distance
+		return deviationSum
+
+	def selection(self):
+#		self.parent1 = None
+#		self.parent2 = None
+		for i in range(len(self.population)):
+			
+			rankI = self.rank(self.population[i])
+			rank1 = float("inf")
+			rank2 = float("inf")
+
+			if self.parent1 is None:
+				self.parent1 = self.population[i]
+			if self.parent2 is None and self.population[i] != self.parent1:
+				self.parent2 = self.population[i]
+
+			if self.parent1 != None:
+				rank1 = self.rank(self.parent1)
+			if self.parent2 != None:
+				rank2 = self.rank(self.parent2)
+
+			if rankI < rank1:
+				self.parent2 = self.parent1
+				self.parent1 = self.population[i]
+
+			if rankI > rank1 and rankI < rank2:
+				self.parent2 = self.population[i]
+#			if self.parent2 != None and self.rank(self.parent2) < self.rank(self.parent1):
+#				aux = self.parent1
+#				self.parent1 = self.parent2
+#				self.parent2 = aux
+
+
+	def evolve(self):
+		#Evolve a Network to have its avoidance the minimal as possible
+
+		#Generates a new random population
+		self.randomPopulate()
+		#Selects the better ones
+		self.selection()
+
+		#Shows the parents rank
+		rank1 = self.rank(self.parent1)
+		print "Parent1: ",rank1
+		rank2 = self.rank(self.parent2)
+		print "Parent2: ",rank2,"\n"
+
+		#saves the present parents
+		lastP1 = self.parent1
+		lastP2 = self.parent2
+		#sets lifetimecounter to 0
+		lifetimeCounter = 0
+
+		self.statistics.append([int(lifetimeCounter), rank1, rank2])
+
+		#Loop for start the evolution process
+		while  rank1 > self.idealFitness:
+
+			#Generate a population with parents
+			self.populate(self.parent1, self.parent2, self.populationSize)
+
+			#Selection happens
+			self.selection()
+
+			print "|"+self.name+"|"
+
+			if lastP1 == self.parent1:
+				lifetimeCounter = lifetimeCounter + 1
+				lastP1 = self.parent1
+			else:
+				lifetimeCounter = lifetimeCounter + 1
+				self.statistics.append([int(lifetimeCounter), rank1, rank2])
+				save("statistics-"+self.name,self.statistics)
+				lastP1 = self.parent1
+
+			if lastP2 == self.parent2:
+				lifetimeCounter = lifetimeCounter + 1
+				lastP2 = self.parent2
+			else:
+				lifetimeCounter = lifetimeCounter + 1
+				self.statistics.append([int(lifetimeCounter), rank1, rank2])
+				save("statistics-"+self.name,self.statistics)
+				lastP2 = self.parent2
+
+			print "||||||||||Parent2|||||||||"
+
+			for i in range(len(self.deservedInputs)):
+				z = self.parent2.activate(self.deservedInputs[i])
+				print "Activation:\t",self.deservedInputs[i],"=",z
+				print "Deserved:\t",self.deservedInputs[i],"=",self.deservedOutputs[i]
+
+			print "||||||||||Parent1|||||||||"
+
+			#Displays each deserved Inputs and its Outputs of the parents Network
+			for i in range(len(self.deservedInputs)):
+				z = self.parent1.activate(self.deservedInputs[i])
+				print "Activation:\t",self.deservedInputs[i],"=",z
+				print "Deserved:\t",self.deservedInputs[i],"=",self.deservedOutputs[i]
+
+			#Loading bar...
+			print "Loading","."*(lifetimeCounter%30)
+			print lifetimeCounter
+
+			#Shows the parents Ranks on the screen
+			rank1 = self.rank(self.parent1)
+			print "Rank Parent1: ",rank1
+			rank2 = self.rank(self.parent2)
+			print "Rank Parent2: ",rank2
+
+			#Overwrite and saves the data of the better parent at each cicle in case the script is interrupted
+			self.parent1.save(self.name+".ann")
+
+			if rank1 <= self.idealFitness:
+				self.statistics.append([int(lifetimeCounter), rank1, rank2])
+				save("statistics-"+self.name,self.statistics)
+				lastP1 = self.parent1
+				break
+
+		self.parent1.save(self.name+".ann")
+
+def save(name, ob):
+	#save object
+	pickle_out = open(name, "wb")
+	pickle.dump(ob, pickle_out)
+	pickle_out.close()
+
+def load(name):
+	pickle_in = open(name, "rb")
+	z = pickle.load(pickle_in)
+	pickle_in.close()
+	return z
+
+
+#Uncomment these lines to test a XOR and NXOR learning
+#gen = Gen("XORXNOR", 2, 3, 2, 2)
+#gen.populationSize = 50
+#gen.deservedInputs = [[1,1],[1,0],[0,1],[0,0]]
+#gen.deservedOutputs = [[0,1],[1,0],[1,0],[0,1]]
+#gen.evolve()
---
+++ neural.py
@@ -0,0 +1,392 @@
+#encoding: utf-8
+import math, random, pickle
+
+class Neuron:
+	"""docstring for Neuron
+		It represents a Neuron processing unity
+	"""
+	def __init__(self, input = [], bend = 0, weight = 1, axon = [], output = 0):
+		if bend > 0.5:	#Controls the direction of the sigmoid function
+			self.bend = 1	#descending	^\_
+		else:
+			self.bend = -1	#ascending	_/^
+		self.weight = weight
+		self.input = input
+		self.output = output
+		self.axon = axon
+
+	def randomize(self):
+		z = random.random()
+		if z > 0.5:
+			self.bend = 1
+		else:
+			self.bend = -1
+		z = random.random()
+		self.weight = z
+
+	def addInput(self, i):
+		self.input.append(i)
+
+	def activate(self):
+		junc = 1 / ( 1 + 10**( -1 * math.e * ( sum(self.input) * 2-1) ) )
+		self.output = 1 / ( 1 + 10**( self.bend * math.e * ( junc * 2-1) ) )
+		return self.output
+
+	def addConnection(self, target, bend = 0, weight = 1):
+		if bend > 0.5:
+			bend = 1
+		else:
+			bend = -1
+		if len(self.axon) < 1:
+			ax = AxonTerminal(bend, weight)
+			ax.input = self
+			ax.output = target
+			self.axon = [ax]
+			return self.axon
+		else:
+			ax = AxonTerminal(bend, weight)
+			ax.input = self
+			ax.output = target
+			self.axon.append(ax)
+			return self.axon
+
+	def communicate(self):
+		for i in self.axon:
+			i.setOutput()
+		return self.axon
+
+class AxonTerminal:
+	"""docstring for AxonTerminal
+		It represents a terminal connection between neuron A and B
+	"""
+	def __init__(self, bend = 0, weight = 1, input = Neuron(), output = Neuron()):
+		if bend > 0.5:
+			self.bend = 1
+		else:
+			self.bend = -1
+		self.weight = weight
+		self.input = input
+		self.output = output
+
+	def randomize(self):
+		z = random.random()
+		if z > 0.5:
+			self.bend = 1
+		else:
+			self.bend = -1
+		z = random.random()
+		self.weight = z
+
+	def setOutput(self):
+		self.output.input.append(1 / ( 1 + 10**( self.bend * math.e * ( self.weight * self.input.output * 2-1) ) ))
+
+class Dendrite:
+	"""docstring for Dendrite
+		It represents a layer of neurons
+	"""
+	def __init__(self, neuronList = [], output = []):
+		self.neuronList = neuronList
+		self.output = output
+
+	def newRandomNeurons(self, qtNeurons):
+		nList = []
+		for i in range(qtNeurons):
+			n = Neuron()
+			nList.append(n)
+		return nList
+
+	def addRandomConnection(self, target):
+		for i in self.neuronList:
+			for j in target.neuronList:
+				i.addConnection(j, random.random(), random.random())
+
+	def addConnection(self, target):
+		for i in self.neuronList:
+			for j in target.neuronList:
+				i.addConnection(j)
+
+	def activate(self):
+		for i in self.neuronList:
+			i.activate()
+
+	def communicate(self):
+		for i in self.neuronList:
+			i.communicate()
+
+	def injectInput(self, input):
+		if len(input) == len(self.neuronList):
+			for i in range(len(self.neuronList)):
+				self.neuronList[i].addInput(input[i])
+			return True
+		else:
+			return False
+
+	def clearIO(self):
+		self.output = []
+		if len(self.neuronList) > 0:
+			for i in (self.neuronList):
+				i.input = []
+				i.output = []
+			return True
+		else:
+			return False
+
+	def setOutput(self):
+		for i in self.neuronList:
+			self.output.append(i.output)
+		return self.output
+		
+class Network:
+	"""docstring for Dendrite
+		It represents a layer of neurons
+	"""
+	def __init__(self, dendriteList = [], input = [], output = []):
+		self.input = input
+		self.dendriteList = dendriteList
+		self.output = output
+
+
+	def newRandom(self, inputHeight, hiddenHeight, hiddenWidth, outputHeight):
+		dendriteList = []
+		nList = []
+		for i in range(inputHeight):
+			n = Neuron([],0,1)
+			nList.append(n)
+		dendriteList.append(Dendrite(nList))
+
+		for i in range(hiddenWidth):
+			nListh = []
+			for i in range(hiddenHeight):
+				n = Neuron()
+				n.randomize
+				nListh.append(n)
+			dendriteList.append(Dendrite(nListh))
+		nListO = []
+		for i in range(outputHeight):
+			n = Neuron()
+			n.randomize
+			nListO.append(n)
+		dendriteList.append(Dendrite(nListO))
+
+		for i in range(1, len(dendriteList)):
+			dendriteList[i-1].addRandomConnection(dendriteList[i])
+		net = Network(dendriteList)
+		return net
+	
+	def save(self, name):
+		#Save the present Network using pickle
+		pickle_out = open(name, "wb")
+		pickle.dump(self, pickle_out)
+		pickle_out.close()
+
+	def load(self, name):
+		#Load another Network set using pickle
+		pickle_in = open(name, "rb")
+		z = pickle.load(pickle_in)
+		pickle_in.close()
+		return z
+
+	def activate(self, input):
+		for i in self.dendriteList:
+			i.clearIO()
+		self.input = input
+		self.injectInput(self.input)
+		for i in self.dendriteList:
+			i.activate()
+			i.communicate()
+		return self.setOutput()
+
+	def injectInput(self, input):
+		self.dendriteList[0].injectInput(input)
+
+	def clearIO(self):
+		self.output = []
+		for i in (self.dendriteList):
+			i.clearIO()
+		return True
+
+	def setOutput(self):
+		self.output = []
+		if self.dendriteList > 0:
+			self.dendriteList[-1].setOutput()
+			self.output = self.dendriteList[-1].output
+			return self.output
+		
+	def show(self):
+		for i in self.dendriteList:
+			print i,"\n HAS :"
+			for j in i.neuronList:
+				print "\n\tNEURON :",j,"\n\t WITH LINKs "
+				for k in j.axon:
+					print "\t\t",k,"\n\t\t\t TO ",k.output
+
+			print "_________________________________"
+
+
+#TESTS
+
+
+
+#Perceptron:
+
+
+
+#Simple AND
+#______________________________________________________
+"""
+#print "Neurons"
+x1 = Neuron()
+x2 = Neuron()
+y1 = Neuron()
+y2 = Neuron()
+
+print x1.axon
+print x1.addConnection(y1, 0, 0.5)
+print x2.axon
+print x2.addConnection(y1, 0, 0.5)
+
+print x1.axon
+print x1.addConnection(y2, 0, 0.6)
+print x2.axon
+print x2.addConnection(y2, 0, 0.6)
+
+#NEURONS ONLY
+#print x1.activate()
+#print x2.activate()
+#print x1.communicate()
+#print x2.communicate()
+
+#print y1.activate()
+#print y2.activate()
+#WITH DENDRITES
+layer1 = Dendrite([x1,x2])
+layer2 = Dendrite([y1,y2])
+
+#layer1.activate()
+#layer1.communicate()
+#layer2.activate()
+#layer2.communicate()
+#print layer2.setOutput()
+
+#WITH NETWORK
+net = Network([layer1,layer2])
+net.clearIO()
+print net.activate([1,1])
+print net.activate([0,1])
+print net.activate([1,0])
+print net.activate([0,0])
+
+
+#______________________________________________________
+
+
+
+#Simple OR
+#______________________________________________________
+
+#print "Neurons"
+#x1 = Neuron([1])
+#x2 = Neuron([1])
+#y = Neuron()
+
+#print x1.axon
+#print x1.addConnection(y, 0, 0.6)
+
+#print x2.axon
+#print x2.addConnection(y, 0, 0.6)
+
+#WITH DENDRITES
+#layer1 = Dendrite([x1,x2])
+#layer2 = Dendrite([y])
+
+#layer1.activate()
+#layer1.communicate()
+#layer2.activate()
+#layer2.communicate()
+#print layer2.setOutput()
+
+#WITHOUT DENDRITES
+#print x1.activate()
+#print x2.activate()
+#print x1.communicate()
+#print x2.communicate()
+
+#print y.input
+#print y.activate()
+
+#______________________________________________________
+
+
+
+#Simple NOT
+#______________________________________________________
+
+#print "Neurons"
+#x1 = Neuron([0])
+#y = Neuron()
+
+#print x1.axon
+#print x1.addConnection(y, 1, 1)
+
+#WITH DENDRITES
+#layer1 = Dendrite([x1])
+#layer2 = Dendrite([y])
+
+#layer1.activate()
+#layer1.communicate()
+#layer2.activate()
+#layer2.communicate()
+#print layer2.setOutput()
+
+#WITHOUT DENDRITES
+#print x1.activate()
+#print x1.communicate()
+
+#print y.input
+#print y.activate()
+
+#______________________________________________________
+
+
+
+#Network TESTS
+#______________________________________________________
+#net = Network()
+#net = net.newRandom(2,3,1,3)
+#print "show"
+#net.show()
+#net.clearIO()
+#print net.activate([1])
+#______________________________________________________
+
+
+O \textit{script} contendo o seguinte código foi executado:\\\\
+x1 = Neuron()	\#Neurônio de entrada\\
+x2 = Neuron()	\#Neurônio de entrada\\
+%
+%Instância dos neurônios da camada de saída:\\
+y1 = Neuron() 	\#Saída para \textit{AND}\\
+y2 = Neuron()	\#Saída para \textit{OR}\\
+%
+%Conecta-se cada neurônio de uma camada a cada neurônio da próxima passando os parametros: neurônio a ser conectado; polarização da conexão; seu peso.\\
+x1.addConnection(y1, 0, 0.5)	\#Conexão entre $x1$ e $y1$\\
+x2.addConnection(y1, 0, 0.5)	\#Conexão entre $x2$ e $y1$\\
+x1.addConnection(y2, 0, 0.6)	\#Conexão entre $x1$ e $y2$\\
+x2.addConnection(y2, 0, 0.6)	\#Conexão entre $x2$ e $y2$\\
+%
+%Instâncias das camadas recebendo a lista de neurônios que as compõem:\\
+layer1 = Dendrite([x1,x2])	\#Instância da camada de entrada\\
+layer2 = Dendrite([y1,y2])	\#Instância da camada de saída\\
+%
+%Instancia da rede recebendo a lista de suas camadas ordenadas:\\
+net = Network([layer1,layer2])	\#Instância da rede\\
+%
+%Limpam-se os valores de entradas e saídas dos componentes:\\
+net.clearIO()	\#Limpeza dos valores de entradas e saídas dos componentes\\
+%
+%Ativa-se a rede imprimindo seus valores de retorno:\\
+print net.activate([1,1])	\#Imprimir o retorno da ativação para a lista no escopo\\
+print net.activate([0,1])	\#Imprimir o retorno da ativação para a lista no escopo\\
+print net.activate([1,0])	\#Imprimir o retorno da ativação para a lista no escopo\\
+print net.activate([0,0])	\#Imprimir o retorno da ativação para a lista no escopo\\
+"""
